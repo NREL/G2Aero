9e7d994696afe128bc1cc3fbdaded8a3c9eeafc5
olgadoronina <olga.doronina@nrel.gov>
2022-05-16 09:21:53 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -14,25 +14,25 @@ if os.path.isfile(requirementPath):
 
 setuptools.setup(
     name='g2aero',
-    version='0.0.1',
+    version='0.1.0',
     packages=setuptools.find_packages(where="src"),
     project_urls={
         "Bug Tracker": "https://github.com/NREL/G2Aero",
     },
-    license='GPLv3',
+    license='BSD 3-Clause License',
     author='Olga Doronina',
     author_email='olga.doronina@nrel.gov',
-    description='Grassmannian shape representation for Aerodynamic Applications',
+    description='Separable shape tensors for aerodynamic applications',
 
     classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Physics',
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+        "License :: OSI Approved :: BSD 3-Clause License"
         'Programming Language :: Python :: 3',
         'Operating System :: OS Independent',
     ],
